5cc214d3d59aedcb61e9ac98b6f4c5894213a908
Ivan Brajković <ivan.brajkovic@epti.com>
2021-10-18 09:55:27 +0200

    setup ts and rollup

---
+++ rollup.config.ts
@@ -0,0 +1,30 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+    terser({
+      compress: false,
+      mangle: false,
+      output: {
+        ecma: 2020,
+        comments: false,
+        beautify: true,
+        indent_level: 2,
+      },
+    }),
+  ],
+};
